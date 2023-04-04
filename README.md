385e4639cb24d2479d1e778629929d016a86438c
Julien cpsn <julien.caposiena@gmail.com>
2023-04-04 20:49:45 +0200

    WIP ELF file reading

---
+++ README.md
@@ -0,0 +1,30 @@
+# Microbit runtime
+
+## Bibliography
+ - https://azeria-labs.com/writing-arm-assembly-part-1/
+
+## Useful commands
+
+Compile from A&AT ASM
+
+```bash
+as assembly.s --32 -o assembly.o
+ld -m elf_i386 -dynamic-linker /lib/ld-linux.so.2 -o assembly -lc assembly.o
+```
+
+Compile from c to binary
+
+```bash
+arm-linux-gnueabihf-gcc -mbe32 -march=armv6 -marm helloworld.c -o helloworld
+```
+
+Compile from c to ASM
+
+```bash
+arm-linux-gnueabihf-gcc -mbe32 -march=armv6 -marm helloworld.c -S
+```
+
+Disassemble program
+```bash
+arm-linux-gnueabihf-objdump -d helloworld
+```
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,42 @@ use std::{env};
 use armv6_m::{MEMORY_MAX_ADDRESSABLE_ADDRESS, R_COUNT};
 use armv6_m::ConditionFlag::PL;
 use armv6_m::Register::{APSR, PC};
+use elfy::Elf;
+use elfy::prelude::SectionData;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let memory = vec![0u8; MEMORY_MAX_ADDRESSABLE_ADDRESS];
+    let mut memory = vec![0u32; MEMORY_MAX_ADDRESSABLE_ADDRESS];
+
+    if args.len() > 1 {
+        let file_path = args.get(1).unwrap();
+        let elf = Elf::load(file_path).expect("Cannot open image");
+
+        let origin = elf.header().entry();
+        println!("origin: {origin}");
+
+        let text_section = elf.try_get_section(".init").expect("The section doesn't exist!");
+
+        if let SectionData::Bytes(bytes) = text_section.data() {
+            for i in (0..bytes.len()).step_by(4) {
+                println!("{:#010b} {:#010b} {:#010b} {:#010b}", bytes[i+3], bytes[i+2], bytes[i+1], bytes[i])
+            }
+        }
+
+        /*
+        let mut origin = file.read_u32::<LittleEndian>().unwrap();
+        origin = swap32(origin);
+
+        let max_read = MEMORY_MAX_ADDRESSABLE_ADDRESS as u32 - origin;
+        println!("{} - {} = {}", MEMORY_MAX_ADDRESSABLE_ADDRESS, origin, max_read);
+
+        */
+        //println!("memory pointer: {}", &p);
+        //memory[p as usize] = 0b101;
+        //println!("data at pointer: {}", &memory[p as usize]);
+        println!();
+    }
 
     // Empty all registers
     let mut reg: [u32; R_COUNT] = [0; R_COUNT];
@@ -27,10 +58,18 @@ fn main() {
 
         println!("op {}", operation as u8);
 
+
         running = false;
     }
 }
 
-fn memory_read(memory: &[u8], address: u32) -> u8 {
+fn memory_read(memory: &[u32], address: u32) -> u32 {
     memory[address as usize]
+}
+
+fn swap32(num: u32) -> u32 {
+    ((num>>24)  & 0xff)     | // move byte 3 to byte 0
+    ((num<<8)   & 0xff0000) | // move byte 1 to byte 2
+    ((num>>8)   & 0xff00)   | // move byte 2 to byte 1
+    ((num<<24)  & 0xff000000)
 }
