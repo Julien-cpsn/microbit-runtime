ae3bf2f92a0f7e53c176103685ac2eb25790f1a9
Julien cpsn <julien.caposiena@gmail.com>
2023-04-10 01:56:46 +0200

    HEX file image load

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::fs::File;
-use std::io::Read;
-use armv6_m::structure::{MAX_ROM, MEMORY_MAX_ADDRESSABLE_ADDRESS, REGISTER_COUNT, ROM_PHYSICAL_ADDRESS};
+use std::io::{Read, stdout, Write};
+use armv6_m::structure::{MAX_RAM, MAX_ROM, MEMORY_MAX_ADDRESSABLE_ADDRESS, REGISTER_COUNT, ROM_PHYSICAL_ADDRESS};
 use armv6_m::structure::ConditionFlag::PL;
 use armv6_m::structure::Register::{APSR, PC};
 use elfy::Elf;
@@ -21,6 +21,9 @@ struct Args {
 
     #[arg(short, long, action)]
     verbose: bool,
+
+    #[arg(short, long, action)]
+    unsafe_mode: bool,
 }
 
 fn main() {
@@ -45,15 +48,42 @@ fn main() {
     }
 
     println!("Bytes written in ROM memory: {}", nb_written_bytes);
+
+    if !args.unsafe_mode {
+        println!("Checking reserved spaces");
+        print!("Code");
+        stdout().flush().ok();
+        // CODE
+        for address in MAX_ROM - 1..0x10000000 {
+            if memory[address as usize] != 0 {
+                panic!("Data wrote in reserved space at address {:X}", address)
+            }
+        }
+
+        print!(", RAM");
+        stdout().flush().ok();
+        // RAM
+        for address in 0x20000000 + MAX_RAM - 1..0x40000000 {
+            if memory[address as usize] != 0 {
+                panic!("Data wrote in reserved space at address {:X}", address)
+            }
+        }
+        // TODO FICR, UIRC, APB Peripherals, AHB Peripherals, Private peripheral bus
+    }
+
     println!();
     println!("==== END INIT ====");
+    println!();
 
     if args.verbose {
         println!("==== START ROM ====");
+        //for i in (0..nb_written_bytes).step_by(2) {
         for i in (0..20).step_by(2) {
             println!(
-                "{:#04x}: {:#010b} {:#010b}",
+                "{:#04x}:\t{:02X}{:02X}\t{:#010b} {:#010b}",
                 i,
+                memory[i],
+                memory[i + 1],
                 memory[i],
                 memory[i + 1]
             )
@@ -62,14 +92,20 @@ fn main() {
     }
     println!();
 
-
     println!("==== START RUNTIME ====");
 
     // Empty all registers
     let mut registers: [u32; REGISTER_COUNT as usize] = [0; REGISTER_COUNT as usize];
 
+    registers[PC.index()] = 0x0000;
+
     let mut running: bool = true;
     while running {
+
+        let instruction = memory_read_u8(&memory, registers[PC.index()] as usize);
+        registers[PC.index()] += 2;
+
+        println!("{:b}", instruction);
 
         running = false;
     }
@@ -99,103 +135,83 @@ fn init_with_file(memory: &mut [u8], file_path: String, verbose: bool, elf: bool
     nb_written_bytes
 }
 
+/// <https://gitlab.com/qemu-project/qemu/-/blob/master/hw/core/loader.c#L1883  >
 /// Reads an Hex file and returns the number of bytes written into the memory
 fn read_hex_file(memory: &mut [u8], file_path: String, verbose: bool) -> usize {
     let file = fs::read_to_string(&file_path).unwrap();
 
     let mut bytes_written: usize = 0;
+    let mut extended_linear_address: u32 = 0x0000000000000000;
     let mut in_v1_block: bool = false;
 
-    if verbose {
-        for (line_index, mut line) in file.split('\n').enumerate() {
-            if line.starts_with(':') {
-                line = line.split_at(1).1;
-                let data = hex::decode(line).unwrap();
+    for (line_index, mut line) in file.split('\n').enumerate() {
+        if line.starts_with(':') {
+            line = line.split_at(1).1;
+            let data = hex::decode(line).unwrap();
 
-                let nb_data: usize = data[0] as usize;
-                let address = u16::from_be_bytes([data[1], data[2]]);
-                let record_type = data[3];
+            let nb_data: usize = data[0] as usize;
+            let address = u16::from_be_bytes([data[1], data[2]]);
+            let record_type = data[3];
 
-                println!("{}: nb: {}, address: {}, type: {:#04X}", line_index, nb_data, address, record_type);
+            if verbose && in_v1_block {
+                println!("{}:\tnb: {},\taddress: {},\ttype: {:#04X}", line_index, nb_data, address, record_type);
+            }
 
+            match record_type {
                 // Block start
-                match record_type {
-                    0x0A => {
-                        if nb_data == 4 {
-                            let bytes_as_u16 = u16::from_be_bytes([data[4], data[5]]);
+                0x0A => {
+                    if nb_data == 4 {
+                        let bytes_as_u16 = u16::from_be_bytes([data[4], data[5]]);
 
-                            // Microbit V1 Block
-                            if bytes_as_u16 == 0x9900 {
-                                in_v1_block = true;
+                        // Microbit V1 Block
+                        if bytes_as_u16 == 0x9900 {
+                            in_v1_block = true;
+
+                            if verbose {
                                 println!("Block start - microbit V1");
                             }
-                            // Microbit V2 Block = 0x9900
-                            if bytes_as_u16 == 0x9903 {
-                                println!("Block start - microbit V2")
+                        }
+                        // Microbit V2 Block = 0x9900
+                        if bytes_as_u16 == 0x9903 {
+
+                            if verbose {
+                                println!("Block start - microbit V2");
                             }
                         }
-                    },
-                    0x0C | 0x0B => {
-                        if nb_data == 0 {
-                            in_v1_block = false;
-                            println!("Block end microbit")
+                    }
+                },
+                // Block end
+                0x0C | 0x0B => {
+                    if nb_data == 0 {
+                        in_v1_block = false;
+
+                        if verbose {
+                            println!("Block end microbit");
                         }
-                    },
-                    0x00 => {
-                        if in_v1_block {
-                            for byte_index in 0..nb_data {
-                                memory[address as usize + byte_index] = data[byte_index + 4];
-                            }
-                            bytes_written += nb_data
+                    }
+                },
+                // Extended linear address
+                0x04 => {
+                    if nb_data == 2 {
+                        extended_linear_address = u32::from_be_bytes([data[4], data[5], 0, 0]);
+                    }
+                    else {
+                        panic!("Malformed data size at line {line_index}")
+                    }
+                },
+                // Data
+                0x00 => {
+                    if in_v1_block {
+                        for byte_index in 0..nb_data {
+                            memory[extended_linear_address as usize + address as usize + byte_index] = data[byte_index + 4];
                         }
-                    },
-                    _ => {}
-                }
-            } else if let Some(character) = line.chars().next() {
-                panic!("Unexpected special char {character} at line {line_index}");
+                        bytes_written += nb_data
+                    }
+                },
+                _ => {}
             }
-        }
-    }
-    else {
-        for (line_index, line) in file.split('\n').enumerate() {
-            if line.starts_with(':') {
-                let data = hex::decode(line.split_at(1).1).unwrap();
-
-                let nb_data: usize = data[0] as usize;
-                let address = u16::from_be_bytes([data[1], data[2]]);
-                let record_type = data[3];
-
-                // Block start
-                match record_type {
-                    0x0A => {
-                        if nb_data == 4 {
-                            let bytes_as_u16 = u16::from_be_bytes([data[4], data[5]]);
-
-                            // Microbit V1 Block
-                            if bytes_as_u16 == 0x9900 {
-                                in_v1_block = true;
-                            }
-                            // Microbit V2 Block = 0x9900
-                        }
-                    },
-                    0x0C | 0x0B => {
-                        if nb_data == 0 {
-                            in_v1_block = false;
-                        }
-                    },
-                    0x00 => {
-                        if in_v1_block {
-                            for byte_index in 0..nb_data {
-                                memory[address as usize + byte_index] = data[byte_index + 4];
-                            }
-                            bytes_written += nb_data
-                        }
-                    },
-                    _ => {}
-                }
-            } else if let Some(character) = line.chars().next() {
-                panic!("Unexpected special char {character} at line {line_index}");
-            }
+        } else if let Some(character) = line.chars().next() {
+            panic!("Unexpected special char {character} at line {line_index}");
         }
     }
 
@@ -253,6 +269,15 @@ fn memory_read_4u8(memory: &[u8], address: usize) -> [u8;4] {
     ]
 }
 
+/// Reads one byte from memory, returned as u16
+fn memory_read_u16(memory: &[u8], address: usize) -> u16 {
+    let byte_array = [
+        memory[address],
+        memory[address + 1]
+    ];
+    u16::from_be_bytes(byte_array)
+}
+
 /// Reads one byte from memory, returned as u32
 fn memory_read_u32(memory: &[u8], address: usize) -> u32 {
     let byte_array = [
@@ -261,7 +286,7 @@ fn memory_read_u32(memory: &[u8], address: usize) -> u32 {
         memory[address + 2],
         memory[address + 3]
     ];
-    as_u32_be(&byte_array)
+    u32::from_be_bytes(byte_array)
 }
 
 fn swap32(num: u32) -> u32 {
